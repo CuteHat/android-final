ba17e07c55948af39f6d2634a1e202c6b42a4d04
Hammerten <gvalishvili@gmail.com>
2020-07-03 04:24:59 +0400

    add login func

--- app/src/main/java/ge/btu/android_final/LoginActivity.kt
+++ app/src/main/java/ge/btu/android_final/LoginActivity.kt
@@ -1,12 +1,46 @@
 package ge.btu.android_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        init()
+    }
+
+    private fun init() {
+        loginButton.setOnClickListener {
+            loginHandler()
+        }
+    }
+
+    private fun loginHandler() {
+        val email = editLoginEmail.text.toString()
+        val password = editLoginPassword.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "გთხოვთ შეავსოთ ყველა ველი", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Toast.makeText(this, "ავტორიზაცია წარმატებულია", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "მოხდა შეცდომა", Toast.LENGTH_SHORT).show()
+            }
     }
 }
